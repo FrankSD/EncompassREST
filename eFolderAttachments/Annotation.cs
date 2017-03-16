1fe2086063b6632fa868952f26ae792f9564d82e
Jesse Weinstock <jweinstock@homesteadfunding.com>
2017-03-16 13:31:35 -0400

    add Annotation

--- eFolderAttachments/Annotation.cs
+++ eFolderAttachments/Annotation.cs
@@ -8,6 +8,13 @@ namespace EncompassREST.eFolderAttachments
 {
     public class Annotation
     {
-
+        public DateTime? dateCreated { get; set; }
+        public string createdBy { get; set; }
+        public string text { get; set; }
+        public int? left { get; set; }
+        public int? top { get; set; }
+        public int? width { get; set; }
+        public int? height { get; set; }
+        public int? visibilityType { get; set; }
     }
 }
